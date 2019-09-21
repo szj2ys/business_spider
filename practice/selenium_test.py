f3fa66e56e28d290697ce00ab61ac6246346111a
szj <1557436482@163.com>
2019-09-21 11:46:13 -0400

    selenium运行chromedriver\phantomjs

--- practice/selenium_test.py
+++ practice/selenium_test.py
@@ -1,23 +1,30 @@
 #-*-coding:utf-8-*-
 from selenium import webdriver
 import time
+import re
 
 
-def spider(url='https://www.baidu.com/'):
+
+def spider(url='https://www.meitulu.com/t/1283/'):
     option = webdriver.ChromeOptions()
     # 这个命令禁止沙箱模式，否则肯能会报错遇到chrome异常。
     option.add_argument('--no-sandbox')
     # 设置启动无界面化
-    # option.add_argument('--headless')
+    option.add_argument('--headless')
     # 注意path
     browser = webdriver.Chrome(executable_path='/home/szj/softwares/chromedriver', options=option)
+    browser.set_window_size(1200, 2800)
     browser.get(url)
-    browser.find_element_by_xpath('//*[@id="kw"]').clear()
-    img_src = browser.find_element_by_xpath('//*[@id="kw"]').send_keys("美女")
-    browser.find_element_by_xpath('//*[@id="su"]').click()
+    # browser.find_element_by_xpath('//*[@id="kw"]').clear()
+    # browser.find_element_by_xpath('//*[@id="kw"]').send_keys("美女")
+    # browser.find_element_by_xpath('//*[@id="su"]').click()
+    browser.save_screenshot("./bing.png")
+    page_source = browser.page_source
+    # title = re.compile("<title>(.*?)</title>",re.S).findall(page_source)
+    # print(title)
 
     time.sleep(3)
-    print(img_src)
+
     browser.quit()
     # 将网页保存为图片
     # browser.get_screenshot_as_file("./bing.png")
--- practice/test.py
+++ practice/test.py
@@ -3,81 +3,38 @@ import os
 import requests
 from lxml import etree
 import random
+import time
 import json
-
-first_urls=[
-      'https://www.meitulu.com/t/yiyang-elly/',
-            'https://www.meitulu.com/t/yuejintong/','https://www.meitulu.com/t/sugar-xiaotianxincc/','https://www.meitulu.com/t/chunxiaoxi/','https://www.meitulu.com/t/1280/',
-            'https://www.meitulu.com/t/1096/','https://www.meitulu.com/t/1214/','https://www.meitulu.com/t/nixiaoyao/','https://www.meitulu.com/t/mengxinyue-candice/',
-            'https://www.meitulu.com/t/1283/','https://www.meitulu.com/t/sugar-xiaotianxincc/','https://www.meitulu.com/t/xiezhixin-sindy/','https://www.meitulu.com/t/1148/',
-            'https://www.meitulu.com/t/manuela/','https://www.meitulu.com/t/1166/','https://www.meitulu.com/t/ningmengc-lemon/','https://www.meitulu.com/t/1214/',
-            'https://www.meitulu.com/t/xiezhixin-sindy/','https://www.meitulu.com/t/wangyuchun/','https://www.meitulu.com/t/linlin-ailin/','https://www.meitulu.com/t/lixingyi/',
-            'https://www.meitulu.com/t/shenmitao-off/','https://www.meitulu.com/t/zhouyanxi/','https://www.meitulu.com/t/wenxinyi/','https://www.meitulu.com/t/1206/','https://www.meitulu.com/t/1148/',
-            'https://www.meitulu.com/t/1347/','https://www.meitulu.com/t/mansulana/','https://www.meitulu.com/t/1206/','https://www.meitulu.com/t/zhouyanxi/','https://www.meitulu.com/t/mufeifei/',
-            'https://www.meitulu.com/t/1348/','https://www.meitulu.com/t/1165/','https://www.meitulu.com/t/1166/','https://www.meitulu.com/t/liuyuer/','https://www.meitulu.com/t/sabrina/',
-            'https://www.meitulu.com/t/guxinyi/','https://www.meitulu.com/t/chenbaola/','https://www.meitulu.com/t/1112/','https://www.meitulu.com/t/foxyini-menghuli/',
-            'https://www.meitulu.com/t/1384/','https://www.meitulu.com/t/1216/'
-      ]
-model_name=''
-data=''
-urls = []
+from selenium import webdriver
+import re
 
 
+url='https://www.lsmpx.com/forum.php?mod=viewthread&tid=19741&extra=&highlight=%E5%BF%83%E5%A6%8D&page=5'
+option = webdriver.ChromeOptions()
+# 这个命令禁止沙箱模式，否则肯能会报错遇到chrome异常。
+option.add_argument('--no-sandbox')
+# 设置启动无界面化
+# option.add_argument('--headless')
+# 注意path
+browser = webdriver.Chrome(executable_path='/home/szj/softwares/chromedriver', options=option)
+browser.get(url)
+# browser.find_element_by_xpath('/html/body/div[2]/div[4]/ul/li[1]/a/img').click()
+print(browser.current_url)
+# 取出网页源码
+# page_source = browser.page_source
 
-user_agent_file= json.load(open('user_agent.json','r'))
+print(browser.find_element_by_class_name("adw").text)
+# print(browser.find_element_by_id("thread-title"))
+img=browser.find_elements_by_xpath('//ul[@class]/node()/img')
+# img=browser.find_elements_by_xpath('//*[@id="thread-pic"]/ul/li[2]/img')
 
-headers = {'User-Agent': random.choice(user_agent_file)}
+print(img)
 
+# nextpage=re.compile('</a> <a class="a1" href="(.*?)">下一页</a>',re.S).findall(page_source)
+# nextpage=re.findall('<a class="a1" href="(.*?)">下一页</a>',page_source)
 
-for first_url in first_urls:
-    if first_url not in urls:
-        urls.append(first_url)
-
-
-for url in urls:
-    data = requests.get(url, headers=headers)
-    response=data.content.decode()
-    # 1.转解析类型
-    xpath_data = etree.HTML(response)
-    # 2调用 xpath的方法,
-    result = xpath_data.xpath('/html/head/title/text()')
-    model_name="".join(result).split('|')[0]
-    print("now download"+model_name)
-    print("url address is :"+url)
-    # create model's name path
-    name_path="/home/szj/spider/meitu/"+model_name+"/"
-    if not os.path.exists(name_path):
-        os.mkdir(name_path)
-
-
-    hrefs=xpath_data.xpath('/html/body/div[2]/div[4]/ul/li/a/@href')
-
-    for i in range(0,len(hrefs)):
-        num=hrefs[i].split("/")[4].split(".")[0]
-        for j in range(1,120):
-            picture_url="https://mtl.ttsqgs.com/images/img/"+num+"/"+str(j)+".jpg"
-
-
-
-
-            # 读取文件json -----list dict
-            downloaded_url_txt = json.load(open('02new.json', 'r'))
-
-            # 将下载过的url追加写入文件夹
-            if picture_url not in str(downloaded_url_txt):
-                picture_response=requests.get(picture_url,headers=headers)
-                picture=picture_response.content
-                # print(type(picture_response.status_code))
-                if (picture_response.status_code != 200):
-                   break
-                picture_name=name_path+num+str(j)+".jpg"
-
-                with open(picture_name,'wb') as f:
-                    f.write(picture)
-                    print(picture_name)
-                    downloaded_url_txt.append(picture_url)
-                    # fp 是 file path
-                    fp = open('02new.json', 'w')
-                    json.dump(downloaded_url_txt, fp)
-                    fp.close()
+# print(page_source)
+time.sleep(2)
+browser.close()
+browser.quit()
 
