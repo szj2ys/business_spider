81ea9000357b483ee92d3c2a965675059634eec5
szj <1557436482@163.com>
2019-09-21 03:16:24 -0400

    selenium运行chromedriver

--- practice/selenium_test.py
+++ practice/selenium_test.py
@@ -3,13 +3,16 @@ from selenium import webdriver
 
 
 
-def spider(url='http://bing.com'):
+def spider(url='https://www.meitulu.com/t/1283/'):
     option = webdriver.ChromeOptions()
     # 这个命令禁止沙箱模式，否则肯能会报错遇到chrome异常。
     option.add_argument('--no-sandbox')
-    option.add_argument('--headless')
+    # 设置启动无界面化
+    # option.add_argument('--headless')
     # 注意path
-    driver = webdriver.Chrome(executable_path='/home/szj/softwares/chromedriver', chrome_options=option)
-    driver.get(url)
-    print(driver.page_source)
+    browser = webdriver.Chrome(executable_path='/home/szj/softwares/chromedriver', options=option)
+    browser.get(url)
+    # 将网页保存为图片
+    browser.get_screenshot_as_file("./bing.png")
+    # print(driver.page_source)
 spider()
