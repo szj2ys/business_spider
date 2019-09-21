7441dd8ee394ff1cff5b8663b81fab01d6db079f
szj <1557436482@163.com>
2019-09-21 03:37:38 -0400

    selenium运行chromedriver

--- practice/selenium_test.py
+++ practice/selenium_test.py
@@ -1,9 +1,9 @@
 #-*-coding:utf-8-*-
 from selenium import webdriver
+import time
 
 
-
-def spider(url='https://www.meitulu.com/t/1283/'):
+def spider(url='https://www.baidu.com/'):
     option = webdriver.ChromeOptions()
     # 这个命令禁止沙箱模式，否则肯能会报错遇到chrome异常。
     option.add_argument('--no-sandbox')
@@ -12,7 +12,14 @@ def spider(url='https://www.meitulu.com/t/1283/'):
     # 注意path
     browser = webdriver.Chrome(executable_path='/home/szj/softwares/chromedriver', options=option)
     browser.get(url)
+    browser.find_element_by_xpath('//*[@id="kw"]').clear()
+    img_src = browser.find_element_by_xpath('//*[@id="kw"]').send_keys("美女")
+    browser.find_element_by_xpath('//*[@id="su"]').click()
+
+    time.sleep(3)
+    print(img_src)
+    browser.quit()
     # 将网页保存为图片
-    browser.get_screenshot_as_file("./bing.png")
+    # browser.get_screenshot_as_file("./bing.png")
     # print(driver.page_source)
 spider()
